1d32757170d56fa2e13d3089da3bb62116477154
GQLiu1001 <moondust1196551872@gmail.com>
2025-03-09 13:19:29 +0800

    项目更新springboot3

---
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -0,0 +1,24 @@
+package com.hmdp.service.impl;
+
+import com.hmdp.entity.Blog;
+import com.hmdp.mapper.BlogMapper;
+import com.hmdp.service.IBlogService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.Resource;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    @Resource
+    private RedisTemplate redisTemplate;
+}
